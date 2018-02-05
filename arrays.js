69ee0deab840462da60ae7b0befc70ff70ceca0b
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-05 21:07:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,6 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
