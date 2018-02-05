9521328d9d4bfa1bcb4b3178157fef0e4bbb0ad5
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-05 19:31:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,5 @@ function addElementToBeginningOfArray(array, element) {
  return (['foo', 1])
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return (['foo', 1])
+  var 
 }
