bc4b07ad560a20743239cd2e3eb7945845dfb8d1
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-05 21:21:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   return array
 }
 function removeElementFromBeginningOfArray (array, element) {
-  array.slice(element)
+  array.slice(1)
   return array
 }
