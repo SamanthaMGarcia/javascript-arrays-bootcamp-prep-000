4f592abb7636e550bd8d341274e747655c4c3989
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-05 19:24:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"] 
 function addElementToBeginningOfArray(array, element) {
- return (['foo', 1])
+ return ([array])
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return (['foo', 1])
